b0fff47b30b8bda6a70de551b7b3e5b7b6243ba5
Hoseong Ahn <hoseong8115@naver.com>
2023-11-30 09:51:23 +0900

    Fix all things

---
+++ script/success.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const wigekdmaImage = document.getElementById("wigekdmaImage");
+  const wigrhddbImage = document.getElementById("wigrhddbImage");
+
+  wigekdmaImage.addEventListener("click", () => {
+    window.location.href = "../index.html";
+  });
+
+  wigrhddbImage.addEventListener("click", () => {
+    takeScreenshot();
+  });
+});
+
+function takeScreenshot() {
+  var canvas = document.getElementById('one');
+  if (canvas.getContext) {
+    var canvasData = canvas.toDataURL("image/png");
+    var a = document.createElement('a');
+    a.href = canvasData;
+    a.download = 'screenshot.png';
+    a.click();
+  } else {
+    alert('귀하의 브라우저에서는 Canvas가 지원되지 않습니다.');
+  }
+}
